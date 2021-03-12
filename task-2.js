3eb036118e5d217bf715002276da57cbfcfe8e71
Irina Murashka <irvil@tut.by>
2021-03-12 22:58:39 +0000

    добавила условия задач в таски

--- task-2.js
+++ task-2.js
@@ -1,3 +1,10 @@
+
+// В HTML есть пустой список ul#ingredients.
+
+// < ul id = "ingredients" ></ul >
+  
+// В JS есть массив строк.
+
 const ingredients = [
   'Картошка',
   'Грибы',
@@ -7,6 +14,7 @@ const ingredients = [
   'Приправы',
 ];
 
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
 
 const listEl = document.querySelector('#ingredients');
 
